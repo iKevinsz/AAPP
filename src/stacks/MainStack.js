3da15be796a55ac76e195a7fe907751c02cd8e96
kevin rodrigo <fioreelokevin@gmail.com>
2024-11-26 22:36:39 -0300

    commit1

---
+++ src/stacks/MainStack.js
@@ -0,0 +1,11 @@
+import react from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
+
+export default() =>(
+    <Stack.Navigator>
+
+    </Stack.Navigator>
+
+)
